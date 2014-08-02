b73aa527a599038861f09092d6be04e11371fc97
lbybee <leland.bybee@gmail.com>
2014-08-02 17:25:44 -0500

    get returns

--- wallet_monitor.py
+++ wallet_monitor.py
@@ -56,3 +56,39 @@ def dataScraper(keyID, vCode, csv_f, t_sleep):
             print datetime.now() - t_1
         except:
             time.sleep(30)
+
+
+def genReturns(data_csv, ret_csv):
+    """generates the returns for the items bought and sold"""
+
+    data_dict = {}
+    reader = csv.reader(open(data_csv, "rb"))
+    
+    # load the data
+    for r in reader:
+        if r[4] not in data_dict:
+            # initalize data dict if not initalized
+            data_dict[r[4]] = {}
+            data_dict[r[4]]["buy"] = {}
+            data_dict[r[4]]["sell"] = {}
+            data_dict[r[4]]["buy"]["totValue"] = 0
+            data_dict[r[4]]["buy"]["totQuantity"] = 0
+            data_dict[r[4]]["sell"]["totValue"] = 0
+            data_dict[r[4]]["sell"]["totQuantity"] = 0
+        # store row value
+        data_dict[r[4]][r[10]]["totValue"] += r[2] * r[5]
+        data_dict[r[4]][r[10]]["totQuantity"] += r[2]
+
+    # store results
+    writer = csv.writer(open(ret_csv, "wb"))
+
+    # now generate the returns
+    for t in data_dict:
+        if (data_dict[t]["buy"]["totValue"] != 0 and
+                data_dict[t]["sell"]["totValue"] != 0):
+            bval = data_dict[t]["buy"]["totValue"]
+            sval = data_dict[t]["sell"]["totValue"]
+            bqnt = data_dict[t]["buy"]["totQuantity"]
+            sqnt = data_dict[t]["sell"]["totQuantity"]
+            writer.writerow([t, sval - bval,
+                             ((sval / sqnt) - (bval / bqnt)) / (bval / bqnt)])
